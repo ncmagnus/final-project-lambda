99a6a36cec33d9bb9b820965e04c5229a6d2f2db
Nina Magnuson <ncmagnus@berkeley.edu>
2018-04-13 15:26:23 -0700

    fixing data simulation Lina

--- ODTR/R/data.simulation.R
+++ ODTR/R/data.simulation.R
@@ -8,29 +8,28 @@
 #' if ODTR-RC you must include kappa for constraint
 #' @param a optional treatment for comparison with A
 #' @param kappa optional numeric indicating proportion of people who can receive treatment
-#' @importFrom stats rnorm rbinom plogis
+#' @importFrom stats rnorm rbinom plogis runif
 #' @export
 data.simulation = function(n, dA = NULL, a = NULL, kappa = NULL){
-
-  # Unobserved variable
-  H = rbinom(n, size = 1, prob = 0.5)
+  # Unobserved variables
+  U.W1 = runif(n, min=0, max=1)
+  U.W2 = rnorm(n, mean=0, sd=1)
+  U.Y = runif(n, min=0, max=1)
 
   # Covariates
-  W1 = rnorm(n, mean = 0, sd = 1)
-  W2 = rnorm(n, mean = 0, sd = 1)
-  W3 = rnorm(n, mean = 0, sd = 1)
-  W4 = rnorm(n, mean = 0, sd = 1)
+  W1 = as.numeric( U.W1 < 0.45)
+  W2 = 0.75*U.W2
+
+  # Blip function
+  Qbar1 = plogis(2*1 + 0.7*W1 - 2*1*W2 - 1*W1*W2)
+  Qbar0 = plogis(2*0 + 0.7*W1 - 2*0*W2 - 0*W1*W2)
+  blip = Qbar1 - Qbar0
 
   # Treatment
   A = rbinom(n, size = 1, prob = 0.5)
 
   # Outcome
-  Y = rbinom(n, size = 1, prob = 0.5*plogis(1-W1^2 + 3*W2 + 5*W3^2*A - 4.45*A)+0.5*plogis(-0.5- W3 + 2*W1*W2 + 3*abs(W2)*A - 1.5*A))
-
-  # Blip function
-  Qbar1 = 0.5*plogis(1-W1^2 + 3*W2 + 5*W3^2*1 - 4.45*1)+0.5*plogis(-0.5- W3 + 2*W1*W2 + 3*abs(W2)*1 - 1.5*1)
-  Qbar0 = 0.5*plogis(1-W1^2 + 3*W2 + 5*W3^2*0 - 4.45*0)+0.5*plogis(-0.5- W3 + 2*W1*W2 + 3*abs(W2)*0 - 1.5*0)
-  blip = Qbar1 - Qbar0
+  Y = as.numeric(U.Y < plogis(2*A + 0.7*W1 - 2*A*W2 - A*W1*W2))
 
   # Treatment under rule
   if (!is.null(dA) & !is.null(a)){
@@ -40,7 +39,7 @@ data.simulation = function(n, dA = NULL, a = NULL, kappa = NULL){
   } else if (!is.null(a)){
     A_star = a
   } else if (dA == "simple dynamic") {
-    A_star = ifelse(W1 > 0, 1, 0)
+    A_star = ifelse(W2 > 0, 1, 0)
   } else if (dA == "ODTR"){
     A_star = as.numeric(blip > 0)
   } else if (dA == "ODTR-RC" & is.null(kappa)){
@@ -53,11 +52,13 @@ data.simulation = function(n, dA = NULL, a = NULL, kappa = NULL){
     A_star = as.numeric(blip > tauP)
   }
 
-  Y_star = rbinom(n, size = 1, prob = 0.5*plogis(1-W1^2 + 3*W2 + 5*W3^2*A_star - 4.45*A_star)+0.5*plogis(-0.5- W3 + 2*W1*W2 + 3*abs(W2)*A_star - 1.5*A_star))
+  # Outcome
+  Y_star = as.numeric( U.Y < plogis(2*A_star + 0.7*W1 - 2*A_star*W2 - A_star*W1*W2))
 
   # Data and target parameter
-  O = data.frame(W1, W2, W3, W4, A, A_star, Y, Y_star)
+  O = data.frame(W1, W2, A, A_star, Y, Y_star)
 
   return(O)
+
 
 }
